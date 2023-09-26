d230a0657f0cc0e7ab9cdfccb257b322ed11e855
Kainan Rodrigues <48391722+Kainanars@users.noreply.github.com>
2023-09-26 13:32:08 -0300

    My first Hello World in TypeScript

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+let message = 'Hello, World!';
+console.log(message);
+let age = 21;
+let firstName = 'Kainan';
+console.log(`My name is ${firstName}, I'm ${age} years old.`);
---
+++ app.ts
@@ -0,0 +1,7 @@
+let message: string = 'Hello, World!';
+console.log(message);
+
+let age: number = 21;
+let firstName: string = 'Kainan';
+
+console.log(`My name is ${firstName}, I'm ${age} years old.`);
